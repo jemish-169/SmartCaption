6b6eb154795feeda89f70c2fd3b254d5a13adebb
jemish-169 <jemishkhunt169@gmail.com>
2025-05-23 14:58:57 +0530

    Implemented navigation and Designed UI

--- app/src/main/java/com/app/smartcaption/core/presentation/main_screen/MainActivity.kt
+++ app/src/main/java/com/app/smartcaption/core/presentation/main_screen/MainActivity.kt
@@ -3,7 +3,6 @@ package com.app.smartcaption.core.presentation.main_screen
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import com.app.smartcaption.core.presentation.navigation.RootNavGraph
 import com.app.smartcaption.theme.SmartCaptionTheme
 
--- app/src/main/java/com/app/smartcaption/features/caption/presentation/CaptionItem.kt
+++ app/src/main/java/com/app/smartcaption/features/caption/presentation/CaptionItem.kt
@@ -1,6 +1,5 @@
 package com.app.smartcaption.features.caption.presentation
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
